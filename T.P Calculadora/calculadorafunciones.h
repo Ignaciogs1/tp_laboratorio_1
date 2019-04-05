84b89ceba3f14c47f6615abbf6d0607970725a75
Ignaciogs1 <ignaciogms@gmail.com>
2019-04-04 22:54:21 -0300

    Finalizado

--- T.P Calculadora/calculadorafunciones.h
+++ T.P Calculadora/calculadorafunciones.h
@@ -3,36 +3,46 @@
 
 int suma (float x,float y);
 /** \brief Recibe los dos operandos que se ingresaron y se calcula la suma de los mismos.
+ *
  * \param Primer operando de tipo float que fue ingresado por el usuario.
  * \param Segundo operando de tipo float que fue ingresado por el usuario.
- * \return Retorna el resultado de la suma de los operandos ingresados.
+ *
+ * \return Retorna el resultado de la suma de los operandos ingresados anteriormente.
  */
 
 int resta (float x,float y);
 /** \brief Recibe los dos operandos que se ingresaron y se calcula la resta de los mismos.
+ *
  * \param Primer operando de tipo float que fue ingresado por el usuario.
  * \param Segundo operando de tipo float que fue ingresado por el usuario.
- * \return Retorna el resultado de la resta de los operandos ingresados.
+ *
+ * \return Retorna el resultado de la resta de los operandos ingresados anteriormente.
  */
 
 float division (float x,float y);
 /** \brief Recibe los dos operandos que se ingresaron y se calcula la división de los mismos.
+ *
  * \param Primer operando de tipo float que fue ingresado por el usuario.
  * \param Segundo operando de tipo float que fue ingresado por el usuario.
- * \return Retorna el resultado de la división de los operandos ingresados.
+ *
+ * \return Retorna el resultado de la división de los operandos ingresados anteriormente.
  */
 
 int multiplicacion (float x,float y);
 /** \brief Recibe los dos operandos que se ingresaron y se calcula la multiplicación de los mismos.
+ *
  * \param Primer operando de tipo float que fue ingresado por el usuario.
  * \param Segundo operando de tipo float que fue ingresado por el usuario.
- * \return Retorna el resultado de la multiplicación de los operandos ingresados.
+ *
+ * \return Retorna el resultado de la multiplicación de los operandos ingresados anteriormente.
  */
 
 long int factorial (float x);
 /** \brief Recibe los dos operandos que se ingresaron y se calcula el factorial de los mismos.
+ *
  * \param Primer operando de tipo float que fue ingresado por el usuario.
- * \return Retorna el factorial de los dos operandos ingresados.
+ *
+ * \return Retorna el factorial de los dos operandos ingresados anteriormente.
  */
 
 #endif // CALCULADORAFUNCIONES_H_INCLUDED
